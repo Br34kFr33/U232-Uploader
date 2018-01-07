c429bd3877ef749f8774c9501b8138ce7b264bfc
Br34kFr33 <33837838+Br34kFr33@users.noreply.github.com>
2018-01-07 11:44:16 -0500

    Update autoup.php
    Fixed a few bugs.

--- autoup.php
+++ autoup.php
@@ -4,13 +4,14 @@ define('UPLOAD_PATH', ROOT_PATH.'/scan');
 define('MOVE_PATH', ROOT_PATH.'/move');
 define('ERROR_PATH', ROOT_PATH.'/error');
 define('TORRENT_PATH', ROOT_PATH.'/torrent');
+define('TEMP_TORRENT', ROOT_PATH.'/temp');
 
 define('LOG_FILE', ROOT_PATH.'/bot.log');
 define('JOB_LOG', ROOT_PATH.'/jobs');
 
-define('SITE_ROOT', 'http://testing.site');
-define('ANNOUNCE_URL', 'http://testing.site/announce.php?torrent_pass=00000001a2d74d8af3afcfa33d941fb2');
-define('Q_LOGIN', 'http://testing.site/pagelogin.php?qlogin=39c961aae388fa1c8306f4235bb370c670ef367c8878c6efd9931e9b7a88f7ae99afc9d90a87ea1b31be15ddf7fbe821');
+define('SITE_ROOT', 'https://test.site');
+define('ANNOUNCE_URL', 'https://test.site/announce.php');
+define('Q_LOGIN', 'https://test.site/pagelogin.php?qlogin=176a4490e19018f0f2ff65526f6ca6dcb5dc8128199656bcc8b2f39c06840f7afb41054c629efe6c156b97c85674a40d');
 
 function move($source, $dest)
 {
@@ -27,22 +28,23 @@ function make_login()
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
 	$rez = curl_exec($ch);
 	if (!$rez) die('Cannot login!');
-	echo file_put_contents(LOG_FILE, 'Cannot login! '.date('m/d/Y h:i:s').'\n', FILE_APPEND);
+	echo file_put_contents(LOG_FILE, 'Cannot login! '.date('m/d/Y h:i:s')."\r\n", FILE_APPEND);
 }
 
 function make_torrent($file)
 {
 	$info = pathinfo($file);
-	$output = TORRENT_PATH.'/'.$info['basename'].'.torrent';
+	$output = TEMP_TORRENT.'/'.$info['basename'].'.torrent';
 	if (file_exists($output)) unlink($output);
 	$cmd = "mktorrent '$file' -o '$output' -a ".ANNOUNCE_URL;
 	exec($cmd);
 	if (file_exists($output)) return $output;
 	else die('Cannot make torrent!');
-	echo file_put_contents(LOG_FILE, "Cannot make $file torrent! ".date('m/d/Y h:i:s')."\n", FILE_APPEND);
+	echo file_put_contents(LOG_FILE, "Cannot make $file torrent! ".date('m/d/Y h:i:s')."\r\n", FILE_APPEND);
 }
 
 function make_upload($file_full, $ext, $new_dir)
@@ -56,18 +58,20 @@ function make_upload($file_full, $ext, $new_dir)
 	$rez = move($file_full, $move_file);
 	if (!$rez) die('Cannot move file!');
 	$torrent = make_torrent($move_file);
-
-	$source = glob($nfo_file.'/*');	
- 	foreach($source as $a) 
-	{
-	$filename = $a;
-	}
+	
+        $source = glob($nfo_file.'/*.nfo');
+	
+	$nfo = 'There was no nfo file found!';
+	foreach ($source as $a) {
+	if (substr(strtolower($a), -4) == '.nfo') {
 	$nfo = file_get_contents($a);
-	$match = array("/[^a-zA-Z0-9-._&=?:'\s]/", "/\s{2,}/");
+	$match = array("/[^a-zA-Z0-9-._&=?:'\/\s]/", "/\s{2,}/");
 	$replace = array("", " ");
 	$nfo = preg_replace($match, $replace, trim($nfo));
+	}
+	}
 
-	$imdb = "";
+        $imdb = "";
     	if (preg_match('/http:\/\/www.imdb.com\/title\/tt[\d]+\//', $nfo, $matches)) 
 	{
         $imdb = $matches[0];
@@ -103,6 +107,7 @@ function test_login()
 	$ch = curl_init($login_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
 	$rez = curl_exec($ch);
 	if (!$rez) make_login();
@@ -115,49 +120,77 @@ function upload_torrent($torrent, $torrent_info, $file)
 	$ch = curl_init($upload_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
 	curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
 	$rez = curl_exec($ch);
-
-	$torrent_info['MAX_FILE_SIZE']=3145728;
+	
+        $torrent_info['MAX_FILE_SIZE']=3145728;
 	$torrent_info['poster']='';	
 	$torrent_info['youtube']='';	
-	$torrent_info['file'] = new CURLFile (TORRENT_PATH.'/'.$file . ".torrent");	
-	$torrent_info['description']='Br0kens Uploader Bot';	
+	$torrent_info['file'] = new CURLFile (TEMP_TORRENT.'/'.$file .".torrent");	
+	$torrent_info['description']='Auto Upload Bot';	
 	$torrent_info['fontfont']='0';
 	$torrent_info['fontsize']='0';
 	$torrent_info['request']='0';
 	$torrent_info['release_group']='none';
 	$torrent_info['strip']=	'strip';
-
-	$fh = fopen(JOB_LOG.'/'.$file, 'a') or die;
+	
+        $fh = fopen(JOB_LOG.'/'.$file, 'a') or die;
 	$string_data = "Name: ".$torrent_info['name'].PHP_EOL."Added: ".date("m/d/Y h:i:s").PHP_EOL."NFO: ".$torrent_info['descr']
 	.PHP_EOL."IMDB: ".$torrent_info['url'].PHP_EOL."Category: ".$torrent_info['type'];
 	fwrite($fh, $string_data);
 	fclose($fh);
-
-	$upload_url = SITE_ROOT.'/takeupload.php';
+	
+        $upload_url = SITE_ROOT.'/takeupload.php';
 	$ch = curl_init($upload_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($ch, CURLOPT_HEADER, 1); 
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $torrent_info);
 	curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
 	$rez = curl_exec($ch);
-
 	if (!$rez || strpos($rez, 'login.php')) 
 	{
 		make_login();
 		goto loged_in;
 	}
 
-	strpos($rez,'Upload failed!') ? file_put_contents(LOG_FILE, "$file failed on ".date("m/d/Y h:i:s")."\n", FILE_APPEND) && move(MOVE_PATH.'/'.$file, ERROR_PATH) :
-	file_put_contents(LOG_FILE, "$file uploaded on ".date("m/d/Y h:i:s")."\n",FILE_APPEND);
+	unlink(TEMP_TORRENT.'/'.$file.".torrent");
+
+	strpos($rez,'Upload failed!') ? file_put_contents(LOG_FILE, "$file failed on ".date("m/d/Y h:i:s")."\r\n", FILE_APPEND) && move(MOVE_PATH.'/'.$file, ERROR_PATH) :
+	file_put_contents(LOG_FILE, "$file uploaded on ".date("m/d/Y h:i:s")."\r\n",FILE_APPEND);
 	//echo $rez;
+	download_torrent($file);
 }
 
-//scan folder for files
+function download_torrent($file) 
+{
+	$search_url = SITE_ROOT.'/browse.php?search='.$file.'&searchin=title&incldead=2';
+	$ch = curl_init($search_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+	$rez = curl_exec($ch);
+
+	preg_match('/download\.php\?torrent=([0-9]+)/', $rez, $sub);
+	//print_r($sub);	
+	$id = $sub[0];
+	
+	$download_url = SITE_ROOT.'/'.$id;
+	$ch = curl_init($download_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+	$rez = curl_exec($ch);
+
+	file_put_contents(TORRENT_PATH.'/'.$file.".torrent", $rez);
+}
+
 function scan_folder()
 {
 	$dir = UPLOAD_PATH;
@@ -179,6 +212,7 @@ function scan_folder()
 		make_upload($file_full, $ext, $dir_done);
 	}
 }
+
 test_login();
 scan_folder();
 ?>
